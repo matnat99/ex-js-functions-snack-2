d93392ce898e885172da36493f8c24ed0d8ae053
Mattia Natale <mattianatale07@gmail.com>
2025-04-07 17:02:28 +0200

    feat: add snack 2

---
+++ Snack 2/script.js
@@ -0,0 +1,10 @@
+/*
+Snack 2
+Crea una arrow function che calcola il quadrato di un numero.
+
+Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+*/
+
+const quadrato = (num) => num * num;
+
+console.log(quadrato(3));
