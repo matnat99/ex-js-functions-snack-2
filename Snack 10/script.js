34093c615955eaa54081f6561365a39d6bb85489
Mattia Natale <mattianatale07@gmail.com>
2025-04-07 19:01:08 +0200

    feat: add snack 10

---
+++ Snack 10/script.js
@@ -0,0 +1,32 @@
+/*
+Snack 10 (Bonus)
+Creare un throttler per limitare l’esecuzione di una funzione
+
+Scrivi una funzione creaThrottler che accetta una funzione e un tempo `limite`.
+Restituisce una nuova funzione che, quando chiamata ripetutamente, esegue l'operazione originale al massimo una volta ogni n millisecondi.
+*/
+
+function stampaMessaggio() {
+  console.log("Messaggio");
+}
+
+function creaThrottler(funzione, limite) {
+  let canExecute = true;
+
+  return function (...argomenti) {
+    if (canExecute) {
+      funzione(...argomenti);
+      canExecute = false;
+
+      setTimeout(() => {
+        canExecute = true;
+      }, limite);
+    }
+  };
+}
+
+const throttled = creaThrottler(stampaMessaggio, 2000);
+
+setInterval(() => {
+  throttled();
+}, 200);
